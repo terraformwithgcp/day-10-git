52a9d1eaa9919ea0ba0d757137f665f1ad4b51e9
himanshu <rathihimanshu@live.com>
2025-12-12 10:45:47 +0530

    two

--- providers.tf
+++ providers.tf
@@ -17,3 +17,9 @@ resource "google_storage_bucket" "action1" {
     location = "US"
   
 }
+
+resource "google_compute_network" "nett1" {
+    name                    = "custom-84848474"
+    auto_create_subnetworks = false
+  
+}
